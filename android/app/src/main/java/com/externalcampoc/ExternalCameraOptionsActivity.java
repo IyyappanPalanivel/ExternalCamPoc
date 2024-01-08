8fca172df3a208db6bf360416451b9548fd09d5e
IyyappanPalanivel <iyyappan.p@technotackle.com>
2024-01-08 23:38:50 +0530

    Basic Camera Preview Skeleton added

--- android/app/src/main/java/com/externalcampoc/ExternalCameraOptionsActivity.java
+++ android/app/src/main/java/com/externalcampoc/ExternalCameraOptionsActivity.java
@@ -1,7 +1,12 @@
 package com.externalcampoc;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.externalcampoc.externalcamera.BasicPreviewActivity;
 
 public class ExternalCameraOptionsActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class ExternalCameraOptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_external_camera_options);
+
+        findViewById(R.id.basic_preview_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BasicPreviewActivity.class));
+            }
+        });
     }
 }
---
+++ android/app/src/main/java/com/externalcampoc/externalcamera/BasicPreviewActivity.java
@@ -0,0 +1,16 @@
+package com.externalcampoc.externalcamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.externalcampoc.R;
+
+public class BasicPreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_preview);
+    }
+}
